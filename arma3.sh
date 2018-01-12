a1c94438d52b48966d8c9e07f868528e470e7ffa
Vincent <agpheaddikilla@yahoo.de>
2018-01-12 20:37:02 +0100

    added workshop server mod loading

--- arma3.sh
+++ arma3.sh
@@ -6,9 +6,9 @@ STEAM_PATH_EXEC="$STEAM_PATH/steamcmd.sh"
 
 process() {
         case $1 in
-        	"install")
-            		install
-            		;;
+        "install")
+            install
+            ;;
                 "start")
                         start
                         ;;
@@ -44,84 +44,88 @@ update() {
 }
 
 install() {
-	if [ ! "$EUID" -ne 0 ]
-	then
-		echo "You only run this script as non-root-user!"
-	else
-		echo "* starting installation"
-		sudo apt-get update
-		echo "* updating packages"
-		sudo apt-get install tmux lib32stdc++6 lib32gcc1 python3 wget
-		echo "* updating steamcmd"
-		if [ ! -d ~/Steam ]
-		then
-			if [ ! -f ~/Steam/steamcmd.sh ]
-			then
-				mkdir ~/Steam
-				cd ~/Steam
-				wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-				tar -xzf steamcmd_linux.tar.gz
-				rm steamcmd_linux.tar.gz
-			fi
-		fi
+    if [ ! "$EUID" -ne 0 ]
+    then
+        echo "You only run this script as non-root-user!"
+    else
+        echo "* starting installation"
+        sudo apt-get update
+        echo "* updating packages"
+        sudo apt-get install tmux lib32stdc++6 lib32gcc1 python3 wget
+        echo "* updating steamcmd"
+        if [ ! -d ~/Steam ]
+        then
+            if [ ! -f ~/Steam/steamcmd.sh ]
+            then
+                mkdir ~/Steam
+                cd ~/Steam
+                wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+                tar -xzf steamcmd_linux.tar.gz
+                rm steamcmd_linux.tar.gz
+            fi
+        fi
 
-		echo "* installing server"
-		update
-		start
-	fi
+        echo "* installing server"
+        update
+        start
+    fi
 }
 
 start() {
-	if ! status; then
-		cd $SERVER_PATH
-		echo "* starting"
+    if ! status; then
+        cd $SERVER_PATH
+        echo "* starting"
 
-		local cMods=""
-		for mod in ${STEAM_WORKSHOP_MODS[@]}
-		do
-			cMods="${cMods}steamapps/workshop/content/107410/$mod;"
-		done
-		for mod in ${SERVER_CLIENT_MODS[@]}
-		do
-			cMods="${cMods}$mod;"
-		done
-		local wMods=$(getWorkshopCollectionMods)
-		for mod in ${wMods[@]}
-		do
-			cMods="${cMods}steamapps/workshop/content/107410/$mod;"
-		done
-		local sMods=""
-		for mod in ${SERVER_SERVER_MODS[@]}
-		do
-			sMods="${sMods}$mod;"
-		done
+        local cMods=""
+        for mod in ${STEAM_CLIENT_WORKSHOP_MODS[@]}
+        do
+                cMods="${cMods}steamapps/workshop/content/107410/$mod;"
+        done
+        for mod in ${SERVER_CLIENT_MODS[@]}
+        do
+                cMods="${cMods}$mod;"
+        done
+        local wMods=$(getWorkshopCollectionMods)
+        for mod in ${wMods[@]}
+        do
+                cMods="${cMods}steamapps/workshop/content/107410/$mod;"
+        done
+        local sMods=""
+	for mod in ${STEAM_SERVER_WORKSHOP_MODS[@]}
+	do
+		sMods="${cMods}steamapps/workshop/content/107410/$mod;"
+	done
+        for mod in ${SERVER_SERVER_MODS[@]}
+        do
+                sMods="${sMods}$mod;"
+        done
 
-		if [ ! -z $cMods ]
-		then
-			cMods="-mod=\"$cMods\""
-		fi
+        if [ ! -z $cMods ]
+        then
+                cMods="-mod=\"$cMods\""
+        fi
 
-		if [ ! -z $sMods ]
-		then
-			sMods="-serverMod=\"$sMods\""
-		fi
+        if [ ! -z $sMods ]
+        then
+                sMods="-serverMod=\"$sMods\""
+        fi
 
-		SERVER_PARAMETERS+=($cMods)
-		SERVER_PARAMETERS+=($sMods)
+        SERVER_PARAMETERS+=($cMods)
+        SERVER_PARAMETERS+=($sMods)
 
-		if [ -f $SERVER_LOG ]
-		then
-			rm $SERVER_LOG
-		fi
+        if [ -f $SERVER_LOG ]
+        then
+                rm $SERVER_LOG
+        fi
 
-		local fCmd="./$SERVER_EXEC_NAME ${SERVER_PARAMETERS[@]} 2>&1 | tee $SERVER_LOG"
-		tmux new -d -s $SERVER_NAME "$fCmd"
-		while ! status; do sleep 0.2; done
+        local fCmd="./$SERVER_EXEC_NAME ${SERVER_PARAMETERS[@]} 2>&1 | tee $SERVER_LOG"
+        tmux new -d -s $SERVER_NAME "$fCmd"
+        while ! status; do sleep 0.2; done
 
-		echo "* started"
-	else
-		echo "! server is already online"
-	fi
+        echo "* started"
+    else
+        echo "! server is already online"
+    fi
 }
 
 stop() {
@@ -134,8 +138,8 @@ stop() {
                 logSave
                 echo "* stopped"
         else
-        	echo "! server is already offline"
-    	fi
+        echo "! server is already offline"
+    fi
 }
 
 #function for shutting down current tmux instance
@@ -153,96 +157,96 @@ restart() {
 
 #status true is on; status false is off
 status() {
-	if ! $(tmux list-sessions | grep -q $SERVER_NAME)
-	then
-		return 1
-	fi
-	return 0
+        if ! $(tmux list-sessions | grep -q $SERVER_NAME)
+        then
+                return 1
+        fi
+        return 0
 }
 
 update() {
-    	$STEAM_PATH_EXEC +login $STEAM_USER $STEAM_PASS +force_install_dir $SERVER_PATH +app_update 233780 validate +quit
+    $STEAM_PATH_EXEC +login $STEAM_USER $STEAM_PASS +force_install_dir $SERVER_PATH +app_update 233780 validate +quit
 }
 
 updateServer() {
-    	stop
-    	update
-	start
+    stop
+    update
+    start
 }
 
 logSave() {
-	if [ ! -f $SERVER_LOG ]
-	then
-		cd $SERVER_PATH
-		local f=$SERVER_PATH/`date +%Y-%m-%d_%H:%M:%S`_dump
-		zip -q $f dump.rpt
-		mv $f $LOG_PATH
-		cd $TOOLS_PATH
-	fi
+        if [ ! -f $SERVER_LOG ]
+        then
+                cd $SERVER_PATH
+                local f=$SERVER_PATH/`date +%Y-%m-%d_%H:%M:%S`_dump
+                zip -q $f dump.rpt
+                mv $f $LOG_PATH
+                cd $TOOLS_PATH
+        fi
 }
 
 getWorkshopCollectionMods() {
-    	echo $(python3 $TOOLS_PATH/getCollectionMods.py $STEAM_API_KEY $STEAM_COLLECTION_ID)
+        echo $(python3 $TOOLS_PATH/getCollectionMods.py $STEAM_API_KEY $STEAM_COLLECTION_ID)
 }
 
 downloadWorkshopMods() {
-	local mods=()
-	mods+=("${STEAM_WORKSHOP_MODS[@]}")
-	mods+=($(getWorkshopCollectionMods))
-	local cmds=()
-	for mod in ${mods[@]}
-	do
-		cmds+=("+workshop_download_item 107410 $mod validate")
-	done
-	#remove the information file about the current mods to update post edited lower-case files too
-	if [ -f $SERVER_PATH/steamapps/workshop/appworkshop_107410.acf ]
-	then
-		rm $SERVER_PATH/steamapps/workshop/appworkshop_107410.acf
-	fi
-	$STEAM_PATH_EXEC +login $STEAM_USER $STEAM_PASS +force_install_dir $SERVER_PATH ${cmds[@]} +quit
-	toLower
+        local mods=()
+	mods+=("${STEAM_SERVER_WORKSHOP_MODS[@]}")
+        mods+=("${STEAM_CLIENT_WORKSHOP_MODS[@]}")
+        mods+=($(getWorkshopCollectionMods))
+        local cmds=()
+        for mod in ${mods[@]}
+        do
+                cmds+=("+workshop_download_item 107410 $mod validate")
+        done
+        #remove the information file about the current mods to update post edited lower-case files too
+        if [ -f $SERVER_PATH/steamapps/workshop/appworkshop_107410.acf ]
+        then
+                rm $SERVER_PATH/steamapps/workshop/appworkshop_107410.acf
+        fi
+        $STEAM_PATH_EXEC +login $STEAM_USER $STEAM_PASS +force_install_dir $SERVER_PATH ${cmds[@]} +quit
+        toLower
 }
 
 updateMods() {
-	stop
-	#download all workshop-related mods
-	downloadWorkshopMods
-	#update all keys
-	local mods=$(getWorkshopCollectionMods)
-	fCmd=()
-	for eK in ${SERVER_KEYS_EXCEPT[@]}
-	do
-		fCmd+=("-not -name '$eK'")
-	done
-	#remove existing keys except a3.bikey and keys listed in SERVER_KEYS_EXCEPT
-	find $SERVER_PATH/keys -type f -not -name 'a3.bikey' ${fCmd[@]} -delete
-	#copy the keys to $SERVER_PATH/keys
-	for mod in ${mods[@]}
-	do
-		local cP="$SERVER_PATH/steamapps/workshop/content/107410/$mod"
-		for f in $(find $cP -type f -name '*.bikey')
-		do
-			cp -u $f $SERVER_PATH/keys
-		done
-	done
-    	start
+        stop
+        #download all workshop-related mods
+        downloadWorkshopMods
+        #update all keys
+        local mods=$(getWorkshopCollectionMods)
+        fCmd=()
+        for eK in ${SERVER_KEYS_EXCEPT[@]}
+        do
+                fCmd+=("-not -name '$eK'")
+        done
+        #remove existing keys except a3.bikey and keys listed in SERVER_KEYS_EXCEPT
+        find $SERVER_PATH/keys -type f -not -name 'a3.bikey' ${fCmd[@]} -delete
+        #copy the keys to $SERVER_PATH/keys
+        for mod in ${mods[@]}
+        do
+                local cP="$SERVER_PATH/steamapps/workshop/content/107410/$mod"
+                for f in $(find $cP -type f -name '*.bikey')
+                do
+                        cp -u $f $SERVER_PATH/keys
+                done
+        done
+    start
 }
 
 toLower() {
-    	find $SERVER_PATH/steamapps/workshop/content/107410/ -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
+        find $SERVER_PATH/steamapps/workshop/content/107410/ -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
 }
 
 help() {
-	echo "####################################################"
-	echo -e "# \e[1;33mHelp menu of Arma 3 Restart Script by Vincent H.\e[0m #"
-	echo "# ------------------------------------------------ #"
-	echo -e "# \e[1;32mstart\e[0m | \e[1;33mStarts the server\e[0m #"
-	echo -e "# \e[1;32mstop\e[0m | \e[1;33mStops the server\e[0m #"
-	echo -e "# \e[1;32mrestart\e[0m | \e[1;33mRestarts the server\e[0m #"
-	echo -e "# \e[1;32mstatus\e[0m | \e[1;33mReturns the current process-PID\e[0m #"
-	echo -e "# \e[1;32mhelp\e[0m | \e[1;33mYou are currently watching the help!\e[0m #"
-	echo "####################################################"
+        echo "####################################################"
+        echo -e "# \e[1;33mHelp menu of Arma 3 Restart Script by Vincent H.\e[0m #"
+        echo "# ------------------------------------------------ #"
+        echo -e "# \e[1;32mstart\e[0m | \e[1;33mStarts the server\e[0m #"
+        echo -e "# \e[1;32mstop\e[0m | \e[1;33mStops the server\e[0m #"
+        echo -e "# \e[1;32mrestart\e[0m | \e[1;33mRestarts the server\e[0m #"
+        echo -e "# \e[1;32mstatus\e[0m | \e[1;33mReturns the current process-PID\e[0m #"
+        echo -e "# \e[1;32mhelp\e[0m | \e[1;33mYou are currently watching the help!\e[0m #"
+        echo "####################################################"
 }
 
-cd $TOOLS_PATH
-process $1 $2
+process $1 $2 cd $MAIN_PATH
